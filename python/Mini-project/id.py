7670c5381ee4d177a9989df645b2a245a3e5ac8b
Nginx0 <slametkopling076@gmail.com>
2024-02-26 21:37:11 +0700

    baru reset hp jadi login ulang deh >//<

--- python/Mini-project/id.py
+++
@@ -1,8 +0,0 @@
-#program identitas
-
-nama = input("nama\t:")
-umur = int(input("umur\t:"))
-alamat = input("alamat\t:")
-
-#hasil
-print(f"\nnama anda {nama} umur {umur} tahun,  alamat di {alamat}.")
